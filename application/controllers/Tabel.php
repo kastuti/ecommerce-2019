eccce6766f2299b9b784e9bc66f4374040afadc6
sony17ekaananda <sony17ekaananda@gmail.com>
2019-07-18 11:42:15 +0700

    update table charts

--- application/controllers/Tabel.php
+++ application/controllers/Tabel.php
@@ -7,9 +7,11 @@ class Tabel extends CI_Controller {
     }
     public function index()
     {
+        $data['content'] = 'V_tabel';
+        $data['menu'] = 'V_menu';
         $data['judul'] = '';
         $data['data_tabel'] = $this->M_tabel->getData();
-        $this->load->view('V_tabel', $data);
+        $this->load->view('V_main', $data);
     }
     public function formAdd()
     {
